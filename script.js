63e87048c17ae33c502e7ba6be45494222cf2535
GDEtrailrunning <gdeconcienciacorporal@gmail.com>
2025-11-09 21:22:56 -0300

    Subo semana 39 y 40

--- script.js
+++ script.js
@@ -5,8 +5,8 @@ document.addEventListener("DOMContentLoaded", function () {
 const dataLine1 = {
   labels: ['Día 1', 'Día 2', 'Dia 3', 'Día 4', 'Día 5'], // Etiquetas de los días
   datasets: [{
-    label: 'Km recorridos en Semana 37',
-    data: [10, 19, 8, 20, 34], // Datos de los km recorridos en cada día
+    label: 'Km recorridos en Semana 39',
+    data: [9, 10, 11, 12, 14], // Datos de los km recorridos en cada día
     borderColor: 'rgba(75, 192, 192, 1)',
     fill: true,
     tension: 0.1
@@ -14,10 +14,10 @@ const dataLine1 = {
 };
 
 const dataLine2 = {
-  labels: ['Día 1', 'Día 2', 'Dia 3', 'Día 4', 'Día 5'], // Etiquetas de los días
+  labels: ['Día 1', 'Día 2', 'Dia 3'/*, 'Día 4', 'Día 5'*/], // Etiquetas de los días
   datasets: [{
-    label: 'Km recorridos en Semana 38',
-    data: [10, 11, 4, 8, 17], // Datos de los km recorridos en cada día
+    label: 'Km recorridos en Semana 40',
+    data: [6, 5, 130], // Datos de los km recorridos en cada día
     borderColor: 'rgba(153, 102, 255, 1)',
     fill: true,
     tension: 0.1
