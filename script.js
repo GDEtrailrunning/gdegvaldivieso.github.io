8184c58055ba3d4465e1a5e89f239cf637f178c1
GDEtrailrunning <gdeconcienciacorporal@gmail.com>
2025-11-24 00:35:08 -0300

    Cambio html, js

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
 document.addEventListener("DOMContentLoaded", function () {
-  actualizarDiasRestantes();
-  setInterval(actualizarDiasRestantes, 24*60*60*1000); // Actualizar diario
+  //actualizarDiasRestantes();
+  //setInterval(actualizarDiasRestantes, 24*60*60*1000); // Actualizar diario
    // Datos para el gráfico de líneas
 const dataLine1 = {
   labels: ['Día 1', 'Día 2', 'Dia 3', 'Día 4', 'Día 5'], // Etiquetas de los días
   datasets: [{
-    label: 'Km recorridos en Semana 39',
-    data: [9, 10, 11, 12, 14], // Datos de los km recorridos en cada día
+    label: 'Km recorridos en Semana 41',
+    data: [12, 8, 10, 9, 16], // Datos de los km recorridos en cada día
     borderColor: 'rgba(75, 192, 192, 1)',
     fill: true,
     tension: 0.1
@@ -14,10 +14,10 @@ const dataLine1 = {
 };
 
 const dataLine2 = {
-  labels: ['Día 1', 'Día 2', 'Dia 3'/*, 'Día 4', 'Día 5'*/], // Etiquetas de los días
+  labels: ['Día 1', 'Día 2', 'Dia 3', 'Día 4', 'Día 5'], // Etiquetas de los días
   datasets: [{
-    label: 'Km recorridos en Semana 40',
-    data: [6, 5, 130], // Datos de los km recorridos en cada día
+    label: 'Km recorridos en Semana 42',
+    data: [14, 18, 8, 12, 15], // Datos de los km recorridos en cada día
     borderColor: 'rgba(153, 102, 255, 1)',
     fill: true,
     tension: 0.1
