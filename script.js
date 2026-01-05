ca92a0d4d29f9c519b41f09a7a0e693eeb06fc6c
GDEtrailrunning <gdeconcienciacorporal@gmail.com>
2026-01-05 01:25:06 -0300

    Cambio archivos en semana 1

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
 document.addEventListener("DOMContentLoaded", function () {
   //actualizarDiasRestantes();
   //setInterval(actualizarDiasRestantes, 24*60*60*1000); // Actualizar diario
-  //Datos para el gráfico de líneas
+   // Datos para el gráfico de líneas
 const dataLine1 = {
-  labels: ['Día 1', 'Día 2', 'Día 3', 'Día 4'], // Etiquetas de los días
+  labels: ['Día 1', 'Día 2', 'Dia 3', 'Día 4', 'Día 5'], // Etiquetas de los días
   datasets: [{
     label: 'Km recorridos en Semana 1',
-    data: [15, 17, 11, 23], // Datos de los km recorridos en cada día
+    data: [13, 13, 8, 11, 20], // Datos de los km recorridos en cada día
     borderColor: 'rgba(75, 192, 192, 1)',
     fill: true,
     tension: 0.1
@@ -14,10 +14,10 @@ const dataLine1 = {
 };
 
 const dataLine2 = {
-  labels: ['Día 1', 'Día 2', 'Día 3', 'Día 4'], // Etiquetas de los días
+  labels: ['Día 1', 'Día 2', 'Dia 3', 'Día 4', 'Día 5'], // Etiquetas de los días
   datasets: [{
-    label: 'Km recorridos en Semana 47',
-    data: [11, 25, 19, 29], // Datos de los km recorridos en cada día
+    label: 'Km recorridos en Semana 42',
+    data: [14, 18, 8, 12, 15], // Datos de los km recorridos en cada día
     borderColor: 'rgba(153, 102, 255, 1)',
     fill: true,
     tension: 0.1
@@ -25,10 +25,7 @@ const dataLine2 = {
 };
 
 // Configuración del gráfico de líneas
-
-
-// Seleccione los contextos de los <canvas> y cree los gráficos
-const myChartLine1 = new Chart(document.getElementById('myChart-line-1'), {
+const configLine = {
   type: 'line',
   data: dataLine1, // Puede cambiar esto a dataLine2 para mostrar el gráfico de la segunda sección
   options: {
@@ -38,9 +35,11 @@ const myChartLine1 = new Chart(document.getElementById('myChart-line-1'), {
       }
     }
   }
-});
+};
 
- const myChartLine2 = new Chart(document.getElementById('myChart-line-2'), {
+// Seleccione los contextos de los <canvas> y cree los gráficos
+const myChartLine1 = new Chart(document.getElementById('myChart-line-1'), configLine);
+const myChartLine2 = new Chart(document.getElementById('myChart-line-2'), {
   type: 'line',
   data: dataLine2,
   options: {
@@ -54,7 +53,7 @@ const myChartLine1 = new Chart(document.getElementById('myChart-line-1'), {
 
 function actualizarDiasRestantes() {
   // Fecha objetivo fija
-  const objetivoDate = new Date('2025-10-11');
+  const objetivoDate = new Date('2025-11-20');
   const currentDate = new Date();
   const timeDiff = objetivoDate - currentDate;
   const days = Math.floor(timeDiff / (1000 * 60 * 60 * 24));
@@ -100,7 +99,6 @@ function actualizarDiasRestantes() {
   document.getElementById("Semana 2").appendChild(video3);
   document.getElementById("Semana 2").appendChild(video4);
 
-
   audios.forEach(audio => {
     audio.addEventListener("play", () => {
       audios.forEach(otherAudio => {
@@ -110,5 +108,5 @@ function actualizarDiasRestantes() {
       });
     });
   });
-  
 });
+
