07247dfd840726ddd0a91620113327e733e4a3a3
GDEtrailrunning <gdeconcienciacorporal@gmail.com>
2026-08-09 22:07:40 -0300

    Subo semana 30 y 31

--- script.js
+++ script.js
@@ -5,8 +5,8 @@ document.addEventListener("DOMContentLoaded", function () {
 const dataLine1 = {
   labels: ['Día 1', 'Día 2', 'Dia 3', 'Día 4', 'Día 5'], // Etiquetas de los días
   datasets: [{
-    label: 'Km recorridos en Semana 28',
-    data: [14, 14, 7, 8, 25], // Datos de los km recorridos en cada día
+    label: 'Km recorridos en Semana 30',
+    data: [12, 13, 10, 15, 23], // Datos de los km recorridos en cada día
     borderColor: 'rgba(75, 192, 192, 1)',
     fill: true,
     tension: 0.1
@@ -16,8 +16,8 @@ const dataLine1 = {
 const dataLine2 = {
   labels: ['Día 1', 'Día 2', 'Dia 3', 'Día 4', 'Día 5'], // Etiquetas de los días
   datasets: [{
-    label: 'Km recorridos en Semana 29',
-    data: [10, 8, 10, 16, 30], // Datos de los km recorridos en cada día
+    label: 'Km recorridos en Semana 31',
+    data: [11, 8, 8, 12, 25], // Datos de los km recorridos en cada día
     borderColor: 'rgba(153, 102, 255, 1)',
     fill: true,
     tension: 0.1
